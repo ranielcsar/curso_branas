f0f27e5c7e2a9f122f8bbc9ec47987627cab170c
ranielcsar <whyfck@gmail.com>
2024-06-09 11:10:54 -0300

    new: refatoração da atividade 1

---
+++ cccat16_1_refac/src/api.ts
@@ -0,0 +1,73 @@
+import crypto from "crypto";
+import express from "express";
+import pgp from "pg-promise";
+import { validate } from "./validateCpf";
+const app = express();
+app.use(express.json());
+
+app.get("/", async function (_, res) {
+  return res.send("online!");
+});
+
+app.post("/signup", async function (req, res) {
+  const connection = pgp()("postgres://postgres:ride123@localhost:5432/ride");
+
+  try {
+    const [acc] = await connection.query(
+      "SELECT * FROM account WHERE email = $1",
+      [req.body.email]
+    );
+    if (acc) {
+      return res.status(409).send({ message: "Usuário já existe" });
+    }
+    if (!req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
+      return res.status(409).send({ message: "Nome inválido" });
+    }
+    if (!req.body.email.match(/^(.+)@(.+)$/)) {
+      return res.status(409).send({ message: "Email inválido" });
+    }
+    if (!validate(req.body.cpf)) {
+      return res.status(409).send({ message: "CPF inválido" });
+    }
+
+    const id = crypto.randomUUID();
+    if (req.body.isDriver) {
+      if (!req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
+        return res.status(409).send({ message: "Placa inválida" });
+      }
+    }
+    await connection.query(
+      "INSERT INTO account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+      [
+        id,
+        req.body.name,
+        req.body.email,
+        req.body.cpf,
+        req.body.carPlate,
+        !!req.body.isPassenger,
+        !!req.body.isDriver,
+      ]
+    );
+    res.json({
+      accountId: id,
+    });
+  } finally {
+    await connection.$pool.end();
+  }
+});
+
+app.get("/accounts", async function (_, res) {
+  const connection = pgp()("postgres://postgres:ride123@localhost:5432/ride");
+  const accounts = await connection.query("SELECT * FROM account");
+  return res.send({ accounts });
+});
+
+app.delete("/account/:id", async function (req, res) {
+  const connection = pgp()("postgres://postgres:ride123@localhost:5432/ride");
+  await connection.query("DELETE FROM account WHERE account_id = $1", [
+    req.params.id,
+  ]);
+  return res.status(200).send({ message: "Account deleted" });
+});
+
+app.listen(3000);
---
+++ cccat16_1_refac/test/api.test.ts
@@ -0,0 +1,82 @@
+import axios from "axios";
+
+axios.defaults.validateStatus = function () {
+  return true;
+};
+
+let accountId: string | null = null;
+
+test("Deve criar uma conta para o passageiro", async function () {
+  const input = {
+    name: "John Doe",
+    email: `john.doe${Math.floor(Math.random() * 1000)}@gmail.com`,
+    cpf: "87748248800",
+    isPassenger: true,
+  };
+  const output = await axios.post("http://localhost:3000/signup", input);
+  expect(output.status).toBe(200);
+  const uuidRegex =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+  expect(output.data).toStrictEqual({
+    accountId: expect.stringMatching(uuidRegex),
+  });
+  accountId = output.data.accountId;
+});
+
+test("Deve retornar mensagem de erro caso CPF seja inválido", async function () {
+  const input = {
+    name: "John Doe",
+    email: `john.doe${Math.floor(Math.random() * 1000)}@gmail.com`,
+    cpf: "12312312312",
+    isPassenger: true,
+  };
+  const output = await axios.post("http://localhost:3000/signup", input);
+  expect(output.status).toBe(409);
+  expect(output.data).toStrictEqual({ message: "CPF inválido" });
+});
+
+test("Deve retornar mensagem de erro caso Email seja inválido", async function () {
+  const input = {
+    name: "John Doe",
+    email: `john.doegmail.com`,
+    cpf: "12312312312",
+    isPassenger: true,
+  };
+  const output = await axios.post("http://localhost:3000/signup", input);
+  expect(output.status).toBe(409);
+  expect(output.data).toStrictEqual({ message: "Email inválido" });
+});
+
+test("Deve retornar mensagem de erro caso Nome seja inválido", async function () {
+  const input = {
+    name: "123Johes",
+    email: `john.doe${Math.floor(Math.random() * 1000)}@gmail.com`,
+    cpf: "12312312312",
+    isPassenger: true,
+  };
+  const output = await axios.post("http://localhost:3000/signup", input);
+  expect(output.status).toBe(409);
+  expect(output.data).toStrictEqual({ message: "Nome inválido" });
+});
+
+test("Deve retornar -4 ao criar conta existente", async function () {
+  const input = {
+    name: "John Doe",
+    email: `noob@gmail.com`,
+    cpf: "87748248800",
+    isPassenger: true,
+  };
+  const output = await axios.post("http://localhost:3000/signup", input);
+  expect(output.status).toBe(409);
+  expect(output.data).toStrictEqual({ message: "Usuário já existe" });
+});
+
+test("Deve deletar uma conta", async function () {
+  const result = await axios.delete(
+    `http://localhost:3000/account/${accountId}`
+  );
+  expect(result.status).toBe(200);
+  expect(result.data).toStrictEqual({
+    message: "Account deleted",
+  });
+});
